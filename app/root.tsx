3e6daaa089e064430728287e93c25baf105674ab
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-03-23 19:23:42 +0800

    Create folder for css

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import stylesheet from "./tailwind.css";
+import stylesheet from "./../css/tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
